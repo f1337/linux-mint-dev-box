cbb5f7483bb9061c67768e285c2652ccf368ed6d
Michael Fleet <f1337@f1337.us>
2016-05-26 13:03:40 -0700

    git prompt for fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,3 +5,32 @@ fortune | fishthink | lolcat
 
 # bell volume
 xset b 80
+
+
+#
+# git prompt config
+#
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set red (set_color red)
+set gray (set_color -o black)
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,5 +1,7 @@
 function fish_prompt
-	printf '%s %s%s%s> ' (whoami) \
+	#set last_status $status
+	printf '%s \a%s%s%s' (whoami) \
 		(set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
-	xkbbell
+	__fish_git_prompt ' (%s)'
+	printf ' $ '
 end
