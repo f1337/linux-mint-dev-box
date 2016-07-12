2df7de6606a49e34f863713184c80051a75934c3
Michael Fleet <f1337@f1337.us>
2016-07-11 23:41:29 -0700

    urxvt, rofi, bobthefish colors; urxvt keybinds

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -23,24 +23,37 @@ set green (set_color green)
 set red (set_color red)
 set gray (set_color -o black)
 
-# Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
 
-# Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
+# powerline colors, courtesy of @benniemosher
+# https://github.com/benniemosher/dotfiles/blob/clean-fish-redo/config/fish/config.fish#L42
+set -g theme_color_scheme user
+set -l colorfg abb2c0
+set -l dircolor 3E4451
+set __color_initial_segment_exit     white red --bold
+set __color_initial_segment_su       white green --bold
+set __color_initial_segment_jobs     white blue --bold
+
+set __color_path                     $dircolor white
+set __color_path_basename            $dircolor white --bold
+set __color_path_nowrite             magenta $colorfg
+set __color_path_nowrite_basename    magenta $colorfg --bold
+
+set __color_repo                     green white
+set __color_repo_work_tree           green white --bold
+set __color_repo_dirty               red white
+set __color_repo_staged              yellow white
+
+set __color_vi_mode_default          brblue $colorfg --bold
+set __color_vi_mode_insert           brgreen $colorfg --bold
+set __color_vi_mode_visual           bryellow $colorfg --bold
+
+set __color_vagrant                  brcyan $colorfg
+set __color_username                 white black
+set __color_rvm                      brmagenta $colorfg --bold
+set __color_virtualfish              brblue $colorfg --bold
+
 
 # plugins
-fundle plugin 'tuvistavie/fish-ssh-agent'
+# fundle plugin 'tuvistavie/fish-ssh-agent'
 fundle plugin 'oh-my-fish/theme-bobthefish'
 fundle init
